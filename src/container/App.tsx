36bf29945f41d8707ff831a47f98b2864531af16
fox.wang <fox3000wang@hotmail.com>
2020-01-11 13:01:46 +0800

    clean code

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Switch, Route } from "react-router";
-import DicomPage from "./DicomPage";
 import VitaPage from "./VitaPage";
 
 class App extends React.Component<any, any> {
@@ -16,7 +15,6 @@ class App extends React.Component<any, any> {
       >
         <Switch>
           <Route path="/" component={VitaPage} />
-          <Route path="/Dicom" component={DicomPage} />
         </Switch>
       </div>
     );
