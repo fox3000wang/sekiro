a7055eabda039decbf85ecb8bd9bde395c0899ce
fox.wang <fox3000wang@hotmail.com>
2020-01-27 16:48:01 +0800

    修复瘦身后报错

--- src/tasks/init-store.ts
+++ src/tasks/init-store.ts
@@ -6,17 +6,11 @@ import {
   compose,
   bindActionCreators
 } from "redux";
-import { routerReducer, routerMiddleware } from "react-router-redux";
 import { createBrowserHistory as createHistory } from "history";
 import reducers from "../reducer";
 import { conbinedActions } from "../action";
 
-const ignoreAction = {
-  SEND_AOC_DATA: true,
-  "finish: SEND_AOC_DATA": true,
-  HANDLER_FOR: true,
-  SET_LEFT_TIME: true
-};
+const ignoreAction = {};
 
 export interface IDependency {
   api: Api;
@@ -31,25 +25,13 @@ export default (context: any) => {
   // history
   const history = createHistory();
 
-  const middlewares = [thunk, routerMiddleware(history)];
-
   const store = createStore(
     combineReducers({
-      ...reducers,
-      router: routerReducer
-    }),
-    composeEnhancers(applyMiddleware(...middlewares))
-  );
-
-  const actions = _.mapValues(conbinedActions, action =>
-    bindActionCreators(action, store.dispatch)
+      ...reducers
+    })
   );
 
   window.__AOC_STORE__ = store;
 
-  window.__GET_ROOM_SUMMARY__ = () => {
-    return getRoomSummary$(store.getState() as any);
-  };
-
-  return Promise.resolve({ ...context, ...{ store, history, actions } });
+  return Promise.resolve({ ...context, ...{ store, history } });
 };
