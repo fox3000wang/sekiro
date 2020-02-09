c907cdfd54568c129b8ce16e16ad6f8f761ec20d
fox.wang <fox3000wang@hotmail.com>
2020-02-09 18:35:58 +0800

    css2 文档初步完成

--- src/container/CSSPage/index.tsx
+++ src/container/CSSPage/index.tsx
@@ -1,5 +1,16 @@
 import * as React from "react";
-import { Background, Layout, Head, Body, Border, Cursor } from "./style";
+import {
+  Background,
+  Layout,
+  Head,
+  Body,
+  Border,
+  Cursor,
+  Font,
+  List,
+  Margin,
+  Padding
+} from "./style";
 
 /**
  * 整理css读书笔记用
@@ -13,6 +24,18 @@ function CSSPage() {
         <Body>
           <Border>border</Border>
           <Cursor>cursor</Cursor>
+          <Font>font</Font>
+          <List>
+            <ul>
+              <li>li1</li>
+              <li>li2</li>
+              <li>li3</li>
+              <li>li4</li>
+            </ul>
+          </List>
+          <Padding>
+            <Margin></Margin>
+          </Padding>
         </Body>
       </Layout>
     </Background>
--- src/container/CSSPage/style.ts
+++ src/container/CSSPage/style.ts
@@ -8,7 +8,8 @@ import * as github from "../../assets/vita/github.png";
  * counter-increment
  * display
  * float
- *
+ * outline
+ * overflow
  */
 
 /**
@@ -56,7 +57,7 @@ export const Body = styled.div`
 
 const Item = styled.div`
   width: 5rem;
-  height: 0.5rem;
+  height: 0.4rem;
 `;
 
 /**
@@ -82,6 +83,7 @@ export const Border = styled(Item)`
   height: 0.5rem;
   color: #fff;
   margin-left: 0.2rem;
+  border-radius: 1rem;
 `;
 
 /**
@@ -128,7 +130,131 @@ export const Cursor = styled(Item)`
  * small-caps	浏览器会显示小型大写字母的字体。
  * inherit	规定应该从父元素继承 font-variant 属性的值。
  *
+ * letter-spacing 字符之间插入多少空间，默认0
+ *
+ * text-align
+ * left	把文本排列到左边。默认值：由浏览器决定。
+ * right	把文本排列到右边。
+ * center	把文本排列到中间。
+ * justify	实现两端对齐文本效果。
+ * inherit	规定应该从父元素继承 text-align 属性的值。
+ *
+ * text-decoration
+ * none	默认。定义标准的文本。
+ * underline	定义文本下的一条线。
+ * overline	定义文本上的一条线。
+ * line-through	定义穿过文本下的一条线。
+ * blink	定义闪烁的文本。
+ * inherit	规定应该从父元素继承
+ *
+ * text-indent 属性规定文本块中首行文本的缩进
+ *
+ * text-transform 属性控制文本的大小写。
+ * none	默认。定义带有小写字母和大写字母的标准的文本。
+ * capitalize	文本中的每个单词以大写字母开头。
+ * uppercase	定义仅有大写字母。
+ * lowercase	定义无大写字母，仅有小写字母。
+ * inherit	规定应该从父元素继承 text-transform 属性的值。
+ *
  */
 export const Font = styled(Item)`
-  font: italic;
+  background: #aaa;
+  font-style: italic;
+  font-size: 0.3rem;
+  font-variant: small-caps;
+  font-weight: bold;
+  letter-spacing: -0.03rem;
+  line-height: 0.4rem;
+  text-align: center;
+  text-decoration: line-through;
+  text-indent: 10%;
+  text-transform: capitalize;
+`;
+
+/**
+ * list-style
+ *
+ * list-style-image
+ * URL	图像的路径。
+ * none	默认。无图形被显示。
+ * inherit	规定应该从父元素继承 list-style-image 属性的值。
+ *
+ * list-style-position
+ * inside	列表项目标记放置在文本以内，且环绕文本根据标记对齐。
+ * outside	默认值。保持标记位于文本的左侧。列表项目标记放置在文本以外，且环绕文本不根据标记对齐。
+ * inherit	规定应该从父元素继承 list-style-position 属性的值。
+ *
+ * list-style-type
+ * none	无标记。
+ * disc	默认。标记是实心圆。
+ * circle	标记是空心圆。
+ * square	标记是实心方块。
+ * decimal	标记是数字。
+ * decimal-leading-zero	0开头的数字标记。(01, 02, 03, 等。)
+ * lower-roman	小写罗马数字(i, ii, iii, iv, v, 等。)
+ * upper-roman	大写罗马数字(I, II, III, IV, V, 等。)
+ * lower-alpha	小写英文字母The marker is lower-alpha (a, b, c, d, e, 等。)
+ * upper-alpha	大写英文字母The marker is upper-alpha (A, B, C, D, E, 等。)
+ * lower-greek	小写希腊字母(alpha, beta, gamma, 等。)
+ * lower-latin	小写拉丁字母(a, b, c, d, e, 等。)
+ * upper-latin	大写拉丁字母(A, B, C, D, E, 等。)
+ * hebrew	传统的希伯来编号方式
+ * armenian	传统的亚美尼亚编号方式
+ * georgian	传统的乔治亚编号方式(an, ban, gan, 等。)
+ * cjk-ideographic	简单的表意数字
+ * hiragana	标记是：a, i, u, e, o, ka, ki, 等。（日文片假名）
+ * katakana	标记是：A, I, U, E, O, KA, KI, 等。（日文片假名）
+ * hiragana-iroha	标记是：i, ro, ha, ni, ho, he, to, 等。（日文片假名）
+ * katakana-iroha	标记是：I, RO, HA, NI, HO, HE, TO, 等。（日文片假名）
+ */
+export const List = styled.div`
+  list-style: circle url(${github});
+`;
+
+/**
+ * margin 外边距
+ * 4个值分别是 12点 3点，6点，9点 顺时针的位置。
+ */
+export const Margin = styled(Item)`
+  margin: 0.1rem 0.2rem 0.3rem 0.4rem;
+  border: 1px #fff solid;
+`;
+
+/**
+ * padding 内边距宽度
+ * 4个值分别是 12点 3点，6点，9点 顺时针的位置。
+ */
+export const Padding = styled.div`
+  padding: 0.1rem 0.2rem 0.3rem 0.4rem;
+  border: 1px #fff solid;
+`;
+
+/**
+ * position
+ *
+ * absolute	生成绝对定位的元素，相对于 static 定位以外的第一个父元素进行定位。
+ *          元素的位置通过 "left", "top", "right" 以及 "bottom" 属性进行规定。
+ * fixed	  生成绝对定位的元素，相对于浏览器窗口进行定位。
+ *          元素的位置通过 "left", "top", "right" 以及 "bottom" 属性进行规定。
+ * relative 生成相对定位的元素，相对于其正常位置进行定位。
+ *          因此，"left:20" 会向元素的 LEFT 位置添加 20 像素。
+ * static	  默认值。没有定位，元素出现在正常的流中（忽略 top, bottom, left, right 或者 z-index 声明）。
+ * inherit	规定应该从父元素继承 position 属性的值。
+ */
+export const Position = styled(Item)`
+  position: absolute;
+  left: 0;
+`;
+
+/**
+ * white-space 属性设置如何处理元素内的空白。
+ * normal	默认。空白会被浏览器忽略。
+ * pre	空白会被浏览器保留。其行为方式类似 HTML 中的 <pre> 标签。
+ * nowrap	文本不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止。
+ * pre-wrap	保留空白符序列，但是正常地进行换行。
+ * pre-line	合并空白符序列，但是保留换行符。
+ * inherit	规定应该从父元素继承 white-space 属性的值。
+ */
+export const WhiteSpace = styled(Item)`
+  white-space: nowrap;
 `;
