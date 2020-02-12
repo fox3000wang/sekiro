d3c54f7530940a0d6b27dd0e9c57accea6698c8d
fox.wang <fox3000wang@hotmail.com>
2020-02-12 11:37:08 +0800

    优化md页面的样式

--- src/consts/route-names.ts
+++
@@ -1,2 +0,0 @@
-export const VITA = "/";
-export const CSS = "/css";
--- src/note/ThePragmaticProgrammer/style.ts
+++ src/note/ThePragmaticProgrammer/style.ts
@@ -5,8 +5,10 @@ export const Background = styled.div`
   display: flex;
   flex-direction: column;
   font-size: 0.2rem;
+  line-height: 0.2rem;
   height: 100%;
   overflow-x: hidden;
   overflow-y: auto;
   -webkit-overflow-scrolling: touch;
+  padding: 0.5rem;
 `;
